2a36a687e677c26bc6ace0bbbfe3ba719ac7cdd7
Vemula Vanshika <vvemula3@gitam.in>
2024-03-06 16:52:26 +0530

    First

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello WOrld!");
+console.log("Connected");
+
+const API_KEY="8c7d1cc05af54248a4b90328240303";
+
+const locationContainer = document.getElequmentById("location");
+console.log(locationContainer);
+
+const dataContainer = document.getElequmentById("data");
+console.log(dataContainer);
+
+const city = "Delhi";
+
+async function getWeather() {    
+  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`; 
+  const result = await fetch(url);
+  console.log(result);
+
+}
+
+getWeather();
