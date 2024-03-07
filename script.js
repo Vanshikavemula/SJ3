205bbf471f5fa5a69bef8901181e4c022e249296
Vemula Vanshika <vvemula3@gitam.in>
2024-03-07 16:59:42 +0530

    3rd

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-console.log("Connected");
+// 1.1 Lets capture a DOM element in HTML using JS
+const button = document.getElementById("btn");
+console.log(button);
 
 const API_KEY="8c7d1cc05af54248a4b90328240303";
 
-const locationContainer = document.getElequmentById("location");
-console.log(locationContainer);
 
-const dataContainer = document.getElequmentById("data");
-console.log(dataContainer);
+ const city = document.getElementById("city");
+ const temp = document.getElementById("temp");
+ const humidity = document.getElementById("humid");
 
-const city = "Delhi";
+  button.addEventListener("click", async(e) => {
+    e.preventDefault();
+    console.log("Clicked")
+    const inputValue = document.getElementById("weatherInput").value;
+    const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${inputValue}&aqi=no`;
+  
 
-async function getWeather() {    
-  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`; 
-  const result = await fetch(url);
-  console.log(result);
+  const response = await fetch(url);
+  const finalData = await response.json();
+console.log(response);
+  console.log(finalData);
 
-}
+  city.innerText = inputValue;
+  temp.innerText = finalData.current.temp_c;
+  humidity.innerText = finalData.current.humidity;
 
-getWeather();
+  
+ 
+
+ 
+ });
